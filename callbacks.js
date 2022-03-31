24e6ccd27a0f66e82f8bc553848c61c668eb1e2e
jrbigmon <junioterriaga0@gmail.com>
2022-03-31 15:19:41 -0300

    Atualização no Callback

--- callbacks.js
+++ callbacks.js
@@ -2,10 +2,10 @@ function adicionarHttp(url) {
     return "http://" + url
 }
 // função que puxa outra função como um parametro e executa ela dentro de si
-function processar(lista,callback){
+function processar(lista, addHttp){
     var lista2 = []
     for(let i = 0; i < lista.length; i++){
-          lista2.push(callback(lista[i]))
+          lista2.push ( addHttp ( lista[i] ) )
     }
     return lista2;
 }

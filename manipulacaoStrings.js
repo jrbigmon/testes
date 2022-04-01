b84f2f95e37886429252385362770d781a3d58e2
jrbigmon <junioterriaga0@gmail.com>
2022-04-01 12:33:34 -0300

    Exercicios de Strings

--- manipulacaoStrings.js
+++ manipulacaoStrings.js
@@ -1,3 +1,4 @@
+// https://blog.dbins.com.br/manipulando-strings-com-javascript
 // Exercicio 1 // Ver o tamanho de uma string
 let string = "Olá Mundo"
 let large = string.length
@@ -26,4 +27,31 @@ let word = 'Mundo'
 let youExist =  conct.indexOf(word)
 youExist > -1? console.log('Position: ' + youExist + ', Search word: ' + word): 'Not exist'
 
-// Exercicio 7 // 
+// Exercicio 7 // verificar se uma string termina com determinada string 
+
+// Exercicio 8 // converter um array de string em uma string
+let array = ['batma', 'miranha', 'super-homi', 'mexilião' ] 
+let arrayString = array.toString()
+console.log(arrayString)
+
+// Exercicio 9 // Substituir uma string.
+let newString = arrayString.replace('batma', 'robin')
+console.log(newString)
+
+// Exercicio 10 // Repetir uma string.
+console.log(newString.repeat(2))
+
+// Exercicio 12 // Utilizar expressões regulares para trabalhar com strings
+//Os caracteres informados entre as barras são as regras que definem o padrão 
+//que queremos localizar. O que está entre chaves [] é o 
+//intervalo de caracteres válidos, que seria letras maiúsculas e 
+//minúsculas [a-zA-Z]. O ^ representa o começo da expressão e o $ é o final. 
+//A regra definida que dizer para procurar qualquer string que comece com qualquer 
+//letra maiuscula ou minuscula, os caracteres podem ser repetir várias vezes.
+//Utilizando o método match, podemos validar se a nossa string segue o padrão desejado. 
+//Perceba que ela recebe como parâmetro a expressão que desejamos avaliar. 
+let string1 = 'codigo'
+let string2 = 'codigo123'
+const stringRegex = /^[a-zA-Z]+$/
+console.log(string1.match(stringRegex)); // True // isso significa que nao contem os caracteres do Regex
+console.log(string2.match(stringRegex)); // Null // isso significa que contem, retornando null

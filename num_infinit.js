4cc355077afcf87dd196b65311f3c27dcc79ff2c
jrbigmon <junioterriaga0@gmail.com>
2022-07-25 15:28:04 -0300

    ok

--- num_infinit.js
+++ num_infinit.js
@@ -1,10 +1,23 @@
 
-    let nums = 150
+    // let nums = 150
     
-    let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
-    for (let num of nums) {
-        if (num > max_num) {
-            max_num += 1
+    // let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+    // for (let num of nums) {
+    //     if (num > max_num) {
+    //         max_num += 1
+    //     }
+    // }
+    // console.assert(max_num)
+
+    const bla = (params) => {
+        let x = [];
+        for(let i = params; i = 1; i--){
+         x.push(i)
         }
-    }
-    console.assert(max_num)
+     //    var y = 1;
+     //    for(let i = 0; i < x.length; i++){
+     //     y = y * x[i];
+     //    }
+     //    return y;
+      }
+      console.log(bla(3))
